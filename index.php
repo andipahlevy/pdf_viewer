0877edb8a56f0500af0bf97e5594bb38bbf0ff10
andipahlevy <andilevi@gmail.com>
2019-08-27 03:27:42 +0700

    tes

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Floating labels example for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/floating-labels/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="floating-labels.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <form class="form-signin" action="proses.php" method="post" enctype="multipart/form-data">
+      <div class="text-center mb-4">
+        <img class="mb-4" src="images/download.jpg" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">PDF Viewer</h1>
+        <p>Choose your file</p>
+      </div>
+
+      <div class="form-label-group">
+        <input type="file" id="inputEmail" name="file" class="form-control" placeholder="pdf" required autofocus>
+        <label for="inputEmail">File pdf</label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" name="upload" type="submit" value="ok">Proses</button>
+    </form>
+  </body>
+</html>
---
+++ proses.php
@@ -0,0 +1,24 @@
+<?php
+if($_POST['upload']){
+	$ekstensi_diperbolehkan	= array('pdf');
+	$nama = $_FILES['file']['name'];
+	$x = explode('.', $nama);
+	$ekstensi = strtolower(end($x));
+	$ukuran	= $_FILES['file']['size'];
+	$file_tmp = $_FILES['file']['tmp_name'];	
+
+	if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
+		if($ukuran < 100440700){			
+			
+			if( move_uploaded_file($file_tmp, 'uploads/filePDFku.pdf') ){
+				header("Location: pdfviewer.php");
+			}else{
+				echo 'GAGAL MENGUPLOAD GAMBAR';
+			}
+		}else{
+			echo 'UKURAN FILE TERLALU BESAR';
+		}
+	}else{
+		echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';
+	}
+}

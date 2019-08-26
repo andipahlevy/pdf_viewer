23c4dee8d913f04cb9a50bc48e58ec5f35da9057
andipahlevy <andilevi@gmail.com>
2019-08-27 03:38:24 +0700

    tes

--- proses.php
+++ proses.php
@@ -1,5 +1,8 @@
 <?php
-ini_set('max_execution_time', 300); 
+ini_set('max_execution_time', 0); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if($_POST['upload']){
 	$ekstensi_diperbolehkan	= array('pdf');
 	$nama = $_FILES['file']['name'];
@@ -10,12 +13,12 @@ if($_POST['upload']){
 
 	if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
 		if($ukuran < 100440700){			
-			
-			if( move_uploaded_file($file_tmp, 'uploads/filePDFku.pdf') ){
+			move_uploaded_file($file_tmp, 'uploads/filePDFku.pdf')
+			// if( move_uploaded_file($file_tmp, 'uploads/filePDFku.pdf') ){
 				header("Location: pdfviewer.php");
-			}else{
-				echo 'GAGAL MENGUPLOAD GAMBAR';
-			}
+			// }else{
+				// echo 'GAGAL MENGUPLOAD GAMBAR';
+			// }
 		}else{
 			echo 'UKURAN FILE TERLALU BESAR';
 		}
